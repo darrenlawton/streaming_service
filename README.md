aac8bc1a9bfd81f767cea1708c161c12e25af1fd
Darren Lawton <darren@darrenlawton.com>
2020-09-01 23:02:48 +1000

    base kensis files added.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # streaming_service
+Summary of what's happening here.
+pxserve, creates requests which are stored in db.
+each morning, need to check for any requests in the db.
+if so, update status to 'in progress' and commence streaming for relevant markets.
+Note, to stream, we will need to lock a key for the day. 
+Once stream is complete, email file to user.
---
+++ src/kinesis_streaming/kinesis_consumer.py
@@ -0,0 +1,74 @@
+import os
+import sys
+import boto3
+import time, datetime
+import pickle
+from botocore.exceptions import ClientError
+
+
+class kinesisConsumer:
+    def __init__(self, stream_name, shard_id, iterator):
+        super().__init__()
+        self.client = boto3.client('kinesis')
+        self.stream_name = stream_name
+        self.shard_id = shard_id
+        self.iterator = iterator
+        self.stream_freq = self.set_frequency()
+
+    @staticmethod
+    def iterate_records(records):
+        for r in records:
+            partition_key = r['PartitionKey']
+            data = pickle.loads(r['Data'])
+
+        yield partition_key, data
+
+    @staticmethod
+    def set_frequency(MillisBehindLatest=0):
+        stream_frequency = dc.CONSUMER_STREAM_FREQ
+
+        try:
+            if MillisBehindLatest > 0:
+                stream_frequency = stream_frequency / dc.CONSUMER_CATCHUP
+        except ClientError as e:
+            print("could not set consumer frequency: {}".format(e))
+            pass
+
+        return stream_frequency
+
+    def run(self, event):
+        """
+        Poll stream for new record and pass to processing method
+        """
+        response = self.client.get_shard_iterator(StreamName=self.stream_name,
+                                                  ShardId=self.shard_id,
+                                                  ShardIteratorType=self.iterator)
+        iteration = response['ShardIterator']
+
+        while not event.is_set():
+            try:
+                response = self.client.get_records(ShardIterator=iteration)
+                records = response['Records']
+
+                if records:
+                    self.process_records(records)
+
+                iteration = response['NextShardIterator']
+                self.stream_freq = self.set_frequency(response['MillisBehindLatest'])
+                time.sleep(self.stream_freq)
+
+            except ClientError as e:
+                print("Error occurred whilst consuming stream {}".format(e))
+                time.sleep(1)
+
+        print("Consumer terminated.")
+
+
+class consumeData(kinesisConsumer):
+    def process_records(self, records):
+        for partition_key, data_blob in self.iterate_records(records):
+            for record in data_blob:
+                try:
+                    pass
+                except KeyError:
+                    pass
